fd26f5e8e4e5ee859d901254c6051213a7f4bf5c
zhuyun <1935589833@qq.com>
2022-01-13 11:29:13 +0800

    2.3.2
    1,更新IS广告库
    2,需要添加几个系统库,libc++abi.tbd,libbz2.1.0.tbd,libresolv.tbd
    3,FBAudienceNetwork使用动态库

--- NewSDKDemo/NewSDKDemo/libs/TeaSDK.framework/Headers/ShowMilkCommon.h
+++ NewSDKDemo/NewSDKDemo/libs/TeaSDK.framework/Headers/ShowMilkCommon.h
@@ -4,8 +4,8 @@
 #import <AuthenticationServices/AuthenticationServices.h>
 NS_ASSUME_NONNULL_BEGIN
 UIKIT_STATIC_INLINE NSString *MY_SDK_VERSION(){
-    NSString *SDKVERSION = @"2.3.1";
-    NSLog(@"TW__12.15版本: = %@",SDKVERSION);
+    NSString *SDKVERSION = @"2.3.2";
+    NSLog(@"TW__1.13版本: = %@",SDKVERSION);
     return SDKVERSION;
 }
 /*
